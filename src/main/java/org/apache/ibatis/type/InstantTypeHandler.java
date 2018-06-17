a84fa4b26bee920f5d7d554f1392ee27f833c9f6
Jeremy Landis <jeremylandis@hotmail.com>
2018-06-17 02:05:34 -0400

    [ci] Enforce formatting from now on

--- src/main/java/org/apache/ibatis/type/InstantTypeHandler.java
+++ src/main/java/org/apache/ibatis/type/InstantTypeHandler.java
@@ -1,5 +1,5 @@
 /**
- *    Copyright 2016 the original author or authors.
+ *    Copyright 2016-2018 the original author or authors.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -28,7 +28,8 @@ import java.time.Instant;
 public class InstantTypeHandler extends BaseTypeHandler<Instant> {
 
   @Override
-  public void setNonNullParameter(PreparedStatement ps, int i, Instant parameter, JdbcType jdbcType) throws SQLException {
+  public void setNonNullParameter(PreparedStatement ps, int i, Instant parameter, JdbcType jdbcType)
+      throws SQLException {
     ps.setTimestamp(i, Timestamp.from(parameter));
   }
 
