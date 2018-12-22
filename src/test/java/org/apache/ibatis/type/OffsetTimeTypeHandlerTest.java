e7aaeb9cbb5757427af4f81e67ef4f5b0df6e54a
Jeremy Landis <jeremylandis@hotmail.com>
2018-12-22 14:18:34 -0500

    [tests] Update test harness to jupiter
    - junit 5.3.2
    - mockito 2.23.4
    - mockito junit jupiter added
    - assertj-core 3.11.1

--- src/test/java/org/apache/ibatis/type/OffsetTimeTypeHandlerTest.java
+++ src/test/java/org/apache/ibatis/type/OffsetTimeTypeHandlerTest.java
@@ -1,5 +1,5 @@
 /**
- *    Copyright 2016-2017 the original author or authors.
+ *    Copyright 2016-2018 the original author or authors.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -18,7 +18,7 @@ package org.apache.ibatis.type;
 import java.sql.Time;
 import java.time.OffsetTime;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
